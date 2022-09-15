52685c570c31f0aba89509de28f1be035547ceca
lovelyfossil <isaacjosue.r.s@gmail.com>
2022-09-15 16:20:38 -0500

    Fix jest config

--- jest.config.js
+++ jest.config.js
@@ -59,7 +59,7 @@ module.exports = {
   // forceCoverageMatch: [],
 
   // A path to a module which exports an async function that is triggered once before all test suites
-  globalSetup: './test/jestGlobalSetup.js',
+  globalSetup: './test/jestGlobalSetup.js'
 
   // A path to a module which exports an async function that is triggered once after all test suites
   // globalTeardown: undefined,
@@ -132,7 +132,7 @@ module.exports = {
   // runner: "jest-runner",
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
-  setupFiles: ['./test/setEnvVars.js']
+  // setupFiles: ['./test/jestGlobalSetup.js']
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   // setupFilesAfterEnv: [],
